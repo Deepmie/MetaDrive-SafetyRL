834bb8568bbb24fa7db574e30c9fe2411b58f40f
wenhan li <deepmiemie@gmail.com>
2026-01-31 00:27:37 +0800

    update file

--- metadrive/custom2_version2/collector.py
+++ metadrive/custom2_version2/collector.py
@@ -6,8 +6,9 @@ class Collector:
     def __init__(self, name: str, freq: int, path_root: str):
         self._path_root: str = path_root
         self._name: str = name; self._freq = freq
+        self._tmp_file_path: str = os.path.join(self._path_root, f'{self._name}.tmp')
         self._file_path: str = os.path.join(self._path_root, f'{self._name}.txt')
-        self._file = open(self._file_path, mode='w', encoding='utf-8')
+        self._tmp_file = open(self._tmp_file_path, mode='w', encoding='utf-8')
         self._buffer: List[float] = list()
         self._idx: int = 0
 
@@ -18,12 +19,16 @@ class Collector:
     
     def _write_in_file(self):
         while len(self._buffer) > 0:
-            self._file.write(str(self._buffer.pop(0)) + '\n')
-            self._file.flush()
+            self._tmp_file.write(str(self._buffer.pop(0)) + '\n')
+            self._tmp_file.flush()
     
+    def _merge_result(self):
+        self._tmp_file.close()
+        os.replace(self._tmp_file_path, self._file_path)
+        
     def close(self):
-        self._write_in_file()
-        self._file.close()
+        self._write_in_file() # 把buffer中的条目全部写入
+        self._merge_result() # 修改.tmp为.txt
 
 
 if __name__ == '__main__':
