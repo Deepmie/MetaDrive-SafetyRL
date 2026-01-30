f5b06528b9b092813a7493764b693d17df9cb8da
wenhan li <deepmiemie@gmail.com>
2026-01-30 20:45:53 +0800

    update file

--- dp_single_version2/get_data.py
+++ dp_single_version2/get_data.py
@@ -22,6 +22,6 @@ if __name__ == '__main__':
     N: int = len(extract_infos)
     for info in extract_infos:
         state: VehicleState = info['vehicle_state']
-        file_writted.write(f'{state.x},{state.y},{state.v},{state.theta}\n')
+        file_writted.write(f'{state.x},{state.y},{state.v},{state.theta},{state.a},{state.delta}\n')
         file_writted.flush()
     file_writted.close()
