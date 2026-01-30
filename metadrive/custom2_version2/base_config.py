17da280eef1825f188e79519652f65d6701bcad2
wenhan li <deepmiemie@gmail.com>
2026-01-31 01:04:29 +0800

    update file

--- metadrive/custom2_version2/base_config.py
+++ metadrive/custom2_version2/base_config.py
@@ -7,7 +7,7 @@ from typing import Dict, Optional, Tuple
 from metadrive.custom2_version2.type import ActionType
 
 METHOD_NAME          = 'rl_mpc_cbf'
-TEST_MODE            = True
+TEST_MODE            = False
 STATE_DIM            = 259
 ACTION_DIM           = 2
 TORCH_DTYPE          = torch.float32
